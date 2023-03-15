822e4f09623b0bf5e30d3fff5a164d55a1cd5947
Kuesung Park <gueit214@naver.com>
2023-03-15 22:34:54 +0900

    refactor : usernickname props로 넘기기

--- src/pages/result.tsx
+++ src/pages/result.tsx
@@ -156,6 +156,7 @@ export default function Result() {
           <FitHobby
             fitHobbyTypes={recommendation.fitHobbyTypes}
             isShow={!isLoading && view === 'fitHobby'}
+            userName={recommendation.user.name}
           />
           <Share
             hobbyType={recommendation.hobbyType}
