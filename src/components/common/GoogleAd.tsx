800e017424380dbb71037b3026841a9b0cc599d3
Kuesung Park <gueit214@naver.com>
2023-03-19 19:31:43 +0900

    refactor: useEffect 코드 추가

--- src/components/common/GoogleAd.tsx
+++ src/components/common/GoogleAd.tsx
@@ -1,11 +1,55 @@
 import { useEffect } from 'react';
-
+declare global {
+  interface Window {
+    adsbygoogle: any;
+  }
+}
 const GoogleAd = () => {
   useEffect(() => {
     if (typeof window !== 'undefined' && window.adsbygoogle) {
       window.adsbygoogle.push({});
     }
   }, []);
+
+  useEffect(() => {
+    var ads = document.getElementsByClassName('adsbygoogle').length;
+    for (var i = 0; i < ads; i++) {
+      try {
+        (ads = window.adsbygoogle || []).push({});
+      } catch (e) {}
+    }
+  }, []);
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production')
+      try {
+        (window.adsbygoogle = window.adsbygoogle || []).push({});
+        console.log('Advertise is pushed');
+      } catch (e) {
+        console.error('AdvertiseError', e);
+      }
+  }, []);
+
+  if (process.env.NODE_ENV !== 'production')
+    return (
+      <div
+        style={{
+          background: '#e9e9e9',
+          color: 'black',
+          fontSize: '18px',
+          fontWeight: 'bold',
+          textAlign: 'center',
+          padding: '16px',
+          display: 'inline-block',
+          width: '320px',
+          height: '50px',
+          marginTop: '10px',
+        }}
+      >
+        광고 표시 영역
+      </div>
+    );
+
   return (
     <div>
       <ins
@@ -20,6 +64,7 @@ const GoogleAd = () => {
         data-ad-slot="1678485541"
         data-full-width-responsive="true"
       ></ins>
+      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
     </div>
   );
 };
